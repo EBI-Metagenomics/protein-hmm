089721af92d48f5688d7c1c5cc4bfcd7e16982ce
Danilo Horta <danilo.horta@pm.me>
2019-09-09 17:04:09 +0100

    nlog fix

--- hseq/hseq/_nlog.py
+++ hseq/hseq/_nlog.py
@@ -2,8 +2,8 @@ def NLOG(probability: float):
     from math import inf, log
 
     if probability == 1.0:
-        return inf
-    if probability == 0.0:
         return 0.0
+    if probability == 0.0:
+        return inf
 
-    return log(-probability)
+    return -log(probability)
