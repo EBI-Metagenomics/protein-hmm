96d153d6c6b127b7da09caf72d4cd9339a677811
Danilo Horta <danilo.horta@pm.me>
2019-09-10 15:52:37 +0100

    rm convert

--- hseq/hseq/__init__.py
+++ hseq/hseq/__init__.py
@@ -1,4 +1,3 @@
-from ._convert import convert
 from ._frame_state import FrameState
 from ._hmm import HMM
 from ._nlog import nlog
@@ -9,7 +8,6 @@ __version__ = "0.0.1"
 __all__ = [
     "__version__",
     "HMM",
-    "convert",
     "SilentState",
     "NormalState",
     "TripletState",
