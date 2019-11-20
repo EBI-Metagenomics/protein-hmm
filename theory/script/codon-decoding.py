3c8a403307dc20a4892cc4de23960ac11cbf7d9c
Danilo Horta <danilo.horta@pm.me>
2019-11-20 15:02:53 +0000

    codon decoding formulae print

--- theory/script/codon-decoding.py
+++ theory/script/codon-decoding.py
@@ -1,19 +1,36 @@
 from dataclasses import dataclass
-from random import randint, seed
-from typing import List, Tuple
+from typing import Any, List, Tuple
+
+from sympy import Add, Mul, Symbol, simplify
 
 
 @dataclass
 class IndelStep:
     act: str
     pos: int
-    lik: str
+    lik: Any
 
     def __repr__(self):
-        return f"{self.act}:{self.pos}"
+        pos = str(self.pos)
+        if self.act == "N":
+            pos = "_"
+        return f"{self.act}:{pos}"
 
 
 IndelPath = Tuple[IndelStep, ...]
+
+e = Symbol("e")
+e1 = Symbol("(1-e)")
+
+z1 = Symbol("z₁")
+z2 = Symbol("z₂")
+z3 = Symbol("z₃")
+z4 = Symbol("z₄")
+z5 = Symbol("z₅")
+
+x1 = Symbol("x₁")
+x2 = Symbol("x₂")
+x3 = Symbol("x₃")
 
 
 def indel_tree_visit(path: IndelPath, seq_len: int, level: int) -> List[IndelPath]:
@@ -24,17 +41,18 @@ def indel_tree_visit(path: IndelPath, seq_len: int, level: int) -> List[IndelPat
     paths: List[IndelPath] = []
 
     if level in [0, 1]:
-        step = Step("N", 0, "(1-e)")
+        step = Step("N", 0, e1)
         paths += indel_tree_visit(path + (step,), seq_len, level + 1)
         for i in range(seq_len):
-            step = Step("D", i, f"(e/{seq_len})")
+            step = Step("D", i, e / seq_len)
+            # step = Step("D", i, f"(e/{seq_len})")
             paths += indel_tree_visit(path + (step,), seq_len - 1, level + 1)
 
     elif level in [2, 3]:
-        step = Step("N", 0, "(1-e)")
+        step = Step("N", 0, e1)
         paths += indel_tree_visit(path + (step,), seq_len, level + 1)
         for i in range(seq_len + 1):
-            step = Step("I", i, f"(e/{seq_len+1})")
+            step = Step("I", i, e / (seq_len + 1))
             paths += indel_tree_visit(path + (step,), seq_len + 1, level + 1)
 
     return paths
@@ -64,58 +82,62 @@ def apply_indel_path(path: IndelPath, seq: Tuple[str, str, str]):
 
 
 def indel_path_prob(path: IndelPath):
-    # ndels = sum(step.act == "D" for step in path)
-    # nins = sum(step.act == "I" for step in path)
-    # nnot = sum(step.act == "N" for step in path)
-    # prob = ["(1-e)"] * nnot
-    # return "*".join(prob)
-    return "*".join(step.lik for step in path)
+    return Mul(*[step.lik for step in path])
 
 
 paths = generate_all_indel_paths()
-print(f"Total number of paths: {len(paths)}")
-seed(53)
-for i in range(10):
-    j = randint(0, len(paths) - 1)
-    path = paths[j]
-    seq = apply_indel_path(path, ("x1", "x2", "x3"))
-    prob = indel_path_prob(path)
-    print(f"{path}: {seq} -> p({prob})")
+CODON = (str(x1), str(x2), str(x3))
 
 
-# class Sym:
-#     def __init__(self, label: str):
-#         self.label = label
-
-#     def __repr__(self):
-#         return self.label
+def path_seq_len(path):
+    return len(apply_indel_path(path, CODON))
 
 
-# def _visit(seq: List[Sym], level: int):
-#     seqs = []
-#     if level in [0, 1]:
-#         seqs += _visit(seq, level + 1)
-#         for i in range(len(seq)):
-#             j = i + 1
-#             seqs += _visit(seq[:i] + seq[j:], level + 1)
-#     elif level in [2, 3]:
-#         seqs += _visit(seq, level + 1)
-#         for i in range(len(seq) + 1):
-#             seqs += _visit(seq[:i] + [Sym("I")] + seq[i:], level + 1)
-#     else:
-#         seqs.append(seq)
-#     return seqs
+def show_probs(paths):
+    expr = []
+    vecz = [z1, z2, z3, z4, z5]
+    vecx = [x1, x2, x3]
+    for path in paths:
+        seq = apply_indel_path(path, CODON)
+        path_prob = indel_path_prob(path)
+
+        cond = []
+        for i in range(len(seq)):
+            z = str(vecz[i])
+            if seq[i] == "I":
+                cond.append(Symbol(f"p({z})"))
+            else:
+                x = seq[i]
+                cond.append(Symbol(f"I({z}={x})"))
+
+        cond_prob = Mul(*cond)
+        expr.append(cond_prob * path_prob)
+        p1 = f"𝜋={path}"
+        p2 = "𝐳={}".format("".join(seq))
+        zstr = "".join(str(z) for z in vecz[: len(seq)])
+        xstr = "".join(str(x) for x in vecx)
+        p3 = "p(𝛑=𝜋)={}".format(simplify(path_prob))
+        p4 = f"p(Z={zstr}|X={xstr}, 𝛑=𝜋) = {simplify(cond_prob)}"
+        p5 = f"p(Z={zstr}|X={xstr}) = " + str(expr[-1])
+        print(f"{p1} leads to {p2};")
+        print(f"    {p3}; {p4};")
+        print(f"    {p5}.")
+
+    print()
+    print(f"p(Z={zstr}|X=x₁x₂x₃) = " + str(simplify(Add(*expr))))
 
 
-# def generate_sequences():
-#     return _visit([Sym("x1"), Sym("x2"), Sym("x3")], 0)
+goal = """Let 𝜋 denote a path through the base-indel process.
+We want to calculate p(Z=𝐳|X=x₁x₂x₃) = ∑_𝜋 p(Z=𝐳,𝛑=𝜋|X=x₁x₂x₃).
 
+Note that p(Z=𝐳,𝛑=𝜋|X=x₁x₂x₃) = p(Z=𝐳|X=x₁x₂x₃,𝛑=𝜋)p(𝛑=𝜋|X=x₁x₂x₃)
+                              = p(Z=𝐳|X=x₁x₂x₃,𝛑=𝜋)p(𝛑=𝜋)
+"""
+print(goal)
 
-# def infer_ndels(seq: List[Sym]):
-#     return 3 - sum(sym.label in ["x1", "x2", "x3"] for sym in seq)
-
-
-# seqs = generate_sequences()
-# for seq in seqs:
-#     ndels = infer_ndels(seq)
-#     print(f"{seq}: {ndels}")
+for i in range(1, 6):
+    print(f"Paths that lead to |𝐳|={i}")
+    print("------------------------")
+    print()
+    show_probs([path for path in paths if path_seq_len(path) == i])
+    print()
